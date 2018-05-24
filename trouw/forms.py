47cb8b59d3862401eb686d2e01aaf3c872b31016
Thomas <thomas.vanneuville@ugent.be>
2018-05-24 15:31:15 +0200

    added veggie to registrationForm

--- trouw/forms.py
+++ trouw/forms.py
@@ -7,6 +7,7 @@ class RegistrationForm(FlaskForm):
     name = StringField('Naam',validators=[DataRequired(), Length(min=2, max=40)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     amount = RadioField('Aantal Personen', choices=[('0', 'Ik kan niet komen'),('1', 'Ik kom'), ('2', 'Ik kom met partner')], validators =[DataRequired()])
+    food = RadioField('Aantal Veggies?', choices=[('0', 'Geen vegetarische maaltijd'),('1', '1 Vegetarische Maaltijd'), ('2', '2 Vegetarische Maaltijden')], validators =[DataRequired()])
     password = PasswordField('Maak een Wachtwoord', validators =[DataRequired()])
     confirm_password = PasswordField('Confirmeer Wachtwoord', validators=[DataRequired(), EqualTo('password')])
     submit= SubmitField('Sign Up')
